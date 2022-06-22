1fb4bb636bbcff163e8e73d15ad6561a9f0bb3b9
Leo Tumwattana <leo.tumwattana@gmail.com>
2022-06-22 21:05:50 +0800

    Initial Commit

---
+++ UITextView+TextKit2-Exception/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  UITextView+TextKit2-Exception
+//
+//  Created by Leo Tumwattana on 22/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UITextView+TextKit2-Exception/UITextView_TextKit2_ExceptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITextView_TextKit2_ExceptionApp.swift
+//  UITextView+TextKit2-Exception
+//
+//  Created by Leo Tumwattana on 22/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct UITextView_TextKit2_ExceptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
